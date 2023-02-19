2960ee9c20c24db86cc5a2bf815b016e5b576b84
Re-Santos <renata.prog@gmail.com>
2023-02-19 16:03:50 -0300

    Alterações CSS, HTML

--- script.js
+++ script.js
@@ -8,40 +8,32 @@ function imc(){
     const peso= document.getElementById ("peso").value;
     const resultado= document.getElementById ("resultado");
 
-    If (nome !== ""  && altura !== "" && peso!== "" ){
+    if (nome !== '' && altura !== '' && peso !== '') {
+
+        const valorIMC = (peso / (altura * altura)).toFixed(1);
+
+        let classificacao = '';
+
+        if (valorIMC < 18.5){
+            classificacao = 'abaixo do peso.';
+        }else if (valorIMC < 25) {
+            classificacao = 'com peso ideal.';
+        }else if (valorIMC < 30){
+            classificacao = 'levemente acima do peso.';
+        }else if (valorIMC < 35){
+            classificacao = 'com obesidade grau 1.';
+        }else if (valorIMC < 40){
+            classificacao = 'com obesidade grau 2.';
+        }else {
+            classificacao = 'com obesidade grau 3.';
+        }
+
+        resultado.textContent = `${nome} seu IMC é ${valorIMC} e você está ${classificacao}`;
         
-        const valorIMC=(peso/(altura*altura)).toFixed(1);
-
-        let classificacao = "";
-    
-        if (imc<18.5){
-            classificacao= "abaixo do peso";
-        }
-        else if(imc<25){
-            classificacao= "com peso ideal";
-        }
-        else if(imc<30){
-            classificacao= "levemente acima do peso";
-        }
-        else if(imc<35){
-            classificacao= "com obesidade grau 1";
-        }
-        else if(imc>40){
-            classificacao= "com obesidade grau 2";
-        }
-        else (imc>40){
-            classificacao= "com obesidade grau 3";
-        }
-
-             resultado.textContent = `${nome} seu IMC é ${valorIMC} e você está ${classificacao}`;
-    }
-     else{
-
-        resultado.textContent="Prencha todos os campos!";
+    }else {
+        resultado.textContent = 'Preencha todos os campos!';
     }
 
 }
 
-calcular.addEventListener(click,imc)
-
-   
+calcular.addEventListener('click', imc);
